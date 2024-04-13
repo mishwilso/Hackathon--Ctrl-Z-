9dc7f0e11baddaff21e884456e64ea9386eeebb1
CFurch <74484458+CFurch@users.noreply.github.com>
2024-04-13 13:53:57 -0400

    definitely works :)

--- game.py
+++ game.py
@@ -3,24 +3,56 @@ Game
 """
 import level as lvl
 
+
 class Game():
 
-    def __init__(self):
-        self.level_num = 1
+    def __init__(self, level=1):
+        self.level_num = level
         self.lives = 3
         self.level_data = lvl.Level(self.level_num)
 
     def check_solution(self, user_solution):
-        if user_solution == self.level_data.solve:
-            self.level_num += 1
-            return True
-        else:
-            self.lives -= 1
+        """
+        runs the user parsed code
+        """
+        if self.parse_code(user_solution) == None:
             return False
 
     def set_level(self, new_level):
         self.level_num = new_level
+        self.level_data = lvl.Level(self.level_num)
 
     def get_level_data(self):
         return self.level_data
+
+    def parse_code(self, code):
+        """
+        return a list of move instructions for the turtle parsed from the user code
+        """
+        moves = []
+
+        moves.append(self.level_data.parsed_pre_code)
+
+        # if the user input is a move command and the next input is an integer
+        if code[0].contains("move"):
+            if isinstance(code[1],(int)):
+                moves.append(code)
+            else:
+                return None
+
+        if code[0].contains("speed"):
+            if code[1] == "=":
+                if isinstance(code[2],(int)):
+                    moves.append(code)
+                else: 
+                    return None
+            else:
+                return None
+        
+
+        
+        return moves
+
+
+
 
