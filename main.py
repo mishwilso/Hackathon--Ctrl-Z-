0b404ed8a2810d1a85f1238c78be56dd0fd29a51
CFurch <74484458+CFurch@users.noreply.github.com>
2024-04-13 10:26:10 -0400

    moved main code to main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import arcade as ac
+import windows as wind
+
+def main():
+    # create the window
+    window = ac.Window(800, 640)
+
+    # create the custom View. Sections are initialized inside the GameView init
+    view = wind.GameView()
+
+    # show the view
+    window.show_view(view)
+
+    # run arcade loop
+    window.run()
 
 
-if __name__ == "__main__":
-    print("Hello World!")
+if __name__ == '__main__':
+    main()
